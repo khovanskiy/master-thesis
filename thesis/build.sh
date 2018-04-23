f4964198d65b40037462856728a27c53b5680987
Victor Khovanskiy <victor.khovanskiy@gmail.com>
2018-04-24 00:31:32 +0300

    build.sh

---
+++ thesis/build.sh
@@ -0,0 +1,4 @@
+pdftk master_thesis.pdf cat 1-2 output title-page.pdf
+pdftk master_thesis.pdf cat 3-4 output specification.pdf
+pdftk master_thesis.pdf cat 5-6 output annotation.pdf
+pdftk master_thesis.pdf cat 7-33 output thesis.pdf
